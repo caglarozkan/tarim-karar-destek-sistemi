15e57be719aeab43978804a2f5214398587b24d2
Caglar Ozkan <ocaglar797@gmail.com>
2026-07-22 15:04:38 +0300

    port no eklendi

--- app/main.py
+++ app/main.py
@@ -27,6 +27,7 @@ from fastapi.middleware.cors import CORSMiddleware
 origins = [
     "http://localhost:5173",
     "http://localhost:3000",
+    "http://localhost:5174"
 ]
 
 app_tarim.add_middleware(
