0f2c2aeb305c55f3f102340101a08e36d2c9501d
Caglar Ozkan <ocaglar797@gmail.com>
2026-07-24 09:54:32 +0300

    .

---
+++ frontend/src/constants/urunler.jsx
@@ -0,0 +1,21 @@
+//ekranda görünen ile dbde kullanılan arasında baglantı kurmak için
+export const URUN_GORUNEN_ADLAR = {
+  "BIBER SIVRI": "Biber (Sivri)",
+  "DOMATES SALKIM": "Domates (Salkım)",
+  "SALATALIK SILOR": "Salatalık (Silor)",
+  "KABAK TAZE": "Kabak (Taze)",
+  "PATLICAN UZUN": "Patlıcan (Uzun)",
+  "SOGAN KURU": "Soğan (Kuru)",
+  "KARPUZ": "Karpuz",
+  "BAKLA": "Bakla",
+  "BEZELYE": "Bezelye",
+  "BROKOLI": "Brokoli",
+  "ISPANAK": "Ispanak",
+  "KARNABAHAR": "Karnabahar",
+  "LAHANA BEYAZ": "Lahana (Beyaz)",
+  "LAHANA KIRMIZI": "Lahana (Kırmızı)",
+  "MARUL": "Marul",
+  "PIRASA": "Pırasa",
+};
+
+export const URUNLER = Object.keys(URUN_GORUNEN_ADLAR);
