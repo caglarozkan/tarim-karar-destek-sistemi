2dad04714346f665b0398738045c230d12a635c2
Caglar Ozkan <ocaglar797@gmail.com>
2026-07-27 23:59:16 +0300

    Frontend ürün ikonları ve fiyat analizi arayüzü güncellendi

---
+++ frontend/src/constants/productImages.js
@@ -0,0 +1,35 @@
+import pepper from "../assets/product_images/pepper.png";
+import tomato from "../assets/product_images/tomato.png";
+import cucumber from "../assets/product_images/cucumber.png";
+import pumpkin from "../assets/product_images/pumpkin.png";
+import eggplant from "../assets/product_images/eggplant.png";
+import onion from "../assets/product_images/onion.png";
+import watermelon from "../assets/product_images/watermelon.png";
+import favaBeans from "../assets/product_images/fava_beans.png";
+import peas from "../assets/product_images/peas.png";
+import broccoli from "../assets/product_images/broccoli.png";
+import spinach from "../assets/product_images/spinach.png";
+import cauliflower from "../assets/product_images/cauliflower.png";
+import cabbage from "../assets/product_images/cabbage.png";
+import purpleCabbage from "../assets/product_images/purple_cabbage.png";
+import lettuce from "../assets/product_images/lettuce.png";
+import leek from "../assets/product_images/leek.png";
+
+export const PRODUCT_IMAGES = {
+  "BIBER SIVRI": pepper,
+  "DOMATES SALKIM": tomato,
+  "SALATALIK SILOR": cucumber,
+  "KABAK TAZE": pumpkin,
+  "PATLICAN UZUN": eggplant,
+  "SOGAN KURU": onion,
+  "KARPUZ": watermelon,
+  "BAKLA": favaBeans,
+  "BEZELYE": peas,
+  "BROKOLI": broccoli,
+  "ISPANAK": spinach,
+  "KARNABAHAR": cauliflower,
+  "LAHANA BEYAZ": cabbage,
+  "LAHANA KIRMIZI": purpleCabbage,
+  "MARUL": lettuce,
+  "PIRASA": leek,
+};
